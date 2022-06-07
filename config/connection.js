0db8fde7c215dab357fc18d80949cc50f53c5cf8
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-06-06 20:41:53 -0500

    created database connection settings

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    }
+  );
+}
+module.exports = sequelize;
