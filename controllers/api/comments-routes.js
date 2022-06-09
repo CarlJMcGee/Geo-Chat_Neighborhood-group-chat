10cc6b1db4a3474eb41903e0dc71bf14e074ae8a
Abyou <emandaabyou@gmail.com>
2022-06-08 20:31:28 -0500

    Adding get comment by ID rout

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -26,11 +26,53 @@ router.get("/", (req, res) => {
       },
     ],
   })
-    .then((databaseCategoryData) => res.json(databaseCategoryData))
+    .then((databaseCommentsData) => res.json(databaseCommentsData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+
+Product.findOne({
+  where: {
+    id: req.params.id,
+  },
+
+  attributes: ["id", "title", "content", "user_id"],
+  // Incorporating Neighborhood and Post data
+  include: [
+    // Incorporating Neighborhood data
+    {
+      models: Neighborhood,
+      attributes: ["name"],
+    },
+
+    // Incorporating Post data
+    {
+      model: Post,
+      attributes: ["content"],
+    },
+  ],
+})
+  .then((databaseCommentsData) => {
+    if (!databaseCommentsData) {
+      // Sending a status 400 message to the user if comment with the given id is not found
+      res
+        .status(400)
+        .json(
+          `Sorry, No comment with id =>: ${req.params.id} has been found! Please check your input and try again!`
+        );
+
+      // Sending the comment to the user if comment with the given id is found
+      return;
+    }
+    // Converting data to json format
+    res.json(databaseCommentsData);
+  })
+  // Sending a status 500 to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 
 module.exports = router;
