b8adb808215f293706f4f3a6eeca4fe620165239
Abyou <emandaabyou@gmail.com>
2022-06-08 19:16:40 -0500

    The basic structure of the route has been designed

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+// The followins will be replaced with the neighborhood-group-chat tables
+const tableOneRoutes = require("./category-routes");
+const tableTwoRoutes = require("./product-routes");
+const tableThreeRoutes = require("./tag-routes");
+
+router.use("/categories", tableOneRoutes);
+router.use("/products", tableTwoRoutes);
+router.use("/tags", tableThreeRoutes);
+
+module.exports = router;
