3318f2fbe06e7213597718910aa34340599421cc
rgiinther <r.giin95@gmail.com>
2022-06-10 14:08:17 -0500

    fixed conflict stuff

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// The followins will be replaced with the neighborhood-group-chat tables
+//The followins will be replaced with the neighborhood-group-chat tables
 const tableOneRoutes = require("./category-routes");
 const tableTwoRoutes = require("./product-routes");
 const tableThreeRoutes = require("./tag-routes");
@@ -12,15 +10,10 @@ router.use("/products", tableTwoRoutes);
 router.use("/tags", tableThreeRoutes);
 
 module.exports = router;
-=======
-=======
->>>>>>> e11a7d1fad3238a27ba7ca8ec330200a60914c9a
+
 router.get("/", (req, res) => {
   res.send("api Routes");
 });
 
 module.exports = router;
-<<<<<<< HEAD
->>>>>>> 161d7164bc8c5055f148868ede76501107afcf99
-=======
->>>>>>> e11a7d1fad3238a27ba7ca8ec330200a60914c9a
+
