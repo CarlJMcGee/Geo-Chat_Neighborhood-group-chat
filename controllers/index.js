b5056059d41dddab32794c0deb779f3b8f8390bb
rgiinther <r.giin95@gmail.com>
2022-06-10 09:04:45 -0500

    merge conflict fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 const homeRoutes = require("./home-routes");
->>>>>>> e11a7d1fad3238a27ba7ca8ec330200a60914c9a
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
@@ -14,14 +11,9 @@ router.use((req, res) => {
 });
 
 module.exports = router;
-<<<<<<< HEAD
-=======
 const homeRoutes = require("./home-routes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 
 module.exports = router;
->>>>>>> 161d7164bc8c5055f148868ede76501107afcf99
-=======
->>>>>>> e11a7d1fad3238a27ba7ca8ec330200a60914c9a
--- models/User.js
+++ models/User.js
@@ -60,18 +60,6 @@ User.init(
         return newUserData;
       },
       async beforeUpdate(updatedUserData) {
-<<<<<<< HEAD
-      beforeCreate: async (newUserData) => {
-        newUserData.dataValues.password = await bcrypt.hash(
-          newUserData.dataValues.password,
-          10
-        );
-        return newUserData;
-      },
-
-      beforeUpdate: async (updatedUserData) => {
-=======
->>>>>>> e11a7d1fad3238a27ba7ca8ec330200a60914c9a
         updatedUserData.password = await bcrypt.hash(
           updatedUserData.password,
           10
