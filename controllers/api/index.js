ca0f5b09cc10d0a48118b3d099a885a3365dcafa
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-06-10 12:42:24 -0500

    removed git leftovers from merge

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 // The followins will be replaced with the neighborhood-group-chat tables
 const tableOneRoutes = require("./category-routes");
 const tableTwoRoutes = require("./product-routes");
@@ -12,15 +10,8 @@ router.use("/products", tableTwoRoutes);
 router.use("/tags", tableThreeRoutes);
 
 module.exports = router;
-=======
-=======
->>>>>>> e11a7d1fad3238a27ba7ca8ec330200a60914c9a
 router.get("/", (req, res) => {
   res.send("api Routes");
 });
 
 module.exports = router;
-<<<<<<< HEAD
->>>>>>> 161d7164bc8c5055f148868ede76501107afcf99
-=======
->>>>>>> e11a7d1fad3238a27ba7ca8ec330200a60914c9a
