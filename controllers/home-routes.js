c3843a4787b95f2ac644b58be50189ade8cc44c7
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-06-12 12:10:47 -0500

    added logout button with script

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,5 @@
+const { Post, User, Comment } = require("../models");
+
 const router = require("express").Router();
 
 router.use("//", (req, res, next) => {
@@ -13,12 +15,50 @@ router.use("/homepage", (req, res, next) => {
   next();
 });
 
+router.use("/dashboard/", (req, res, next) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/homepage");
+    return;
+  }
+  next();
+});
+
 router.get("/homepage", (req, res) => {
   res.render("homepage");
 });
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+router.get("/dashboard", async (req, res) => {
+  const posts = await Post.findAll({
+    where: {
+      neighborhood_id: req.session.neighborhoodId,
+    },
+    include: [
+      {
+        model: User,
+        as: "OP",
+        attributes: {
+          exclude: ["password"],
+        },
+      },
+      {
+        model: Comment,
+        include: {
+          model: User,
+          as: "commenter",
+          attributes: {
+            exclude: ["password"],
+          },
+        },
+      },
+    ],
+  });
+
+  console.log(posts);
+
+  res.render("dashboard", {
+    loggedIn: req.session.loggedIn,
+    posts: posts,
+  });
 });
 
 router.get("/login", (req, res) => {
