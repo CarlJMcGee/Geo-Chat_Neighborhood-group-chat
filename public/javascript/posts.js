e6fb068420d0580974b6c81a2e771b4a2c2b6ee1
Christopher <leechrs1@gmail.com>
2022-06-13 20:07:36 -0500

    post js

--- public/javascript/posts.js
+++ public/javascript/posts.js
@@ -7,9 +7,9 @@ const commentFormHandler = async (e) => {
 
   const post_title = document.querySelector("#title-edit").value.trim();
 
-  const post_id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 2
-  ];
+  // const post_id = window.location.toString().split("/")[
+  //   window.location.toString().split("/").length - 2
+  // ];
 
   if (post_text) {
     const response = await fetch(`/api/posts/${post_id}`, {
@@ -30,7 +30,7 @@ const commentFormHandler = async (e) => {
   }
 
   if (post_text) {
-    const response = await fetch(`/api/posts/${post_id}`, {
+    const response = await fetch(`/api/posts/`, {
       method: "POST",
       body: JSON.stringify({
         title: post_title,
@@ -40,9 +40,9 @@ const commentFormHandler = async (e) => {
         "Content-Type": "application/json",
       },
     });
-    
+
     if (response.ok) {
-      document.location.replace(`/post/${post_id}`);
+      document.location.replace(`/post/`);
     } else {
       alert(response.statusText);
     }
