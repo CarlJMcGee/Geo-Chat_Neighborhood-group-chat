1043369f000717394515521f490e6707c833867e
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-06-14 20:35:32 -0500

    fixed signup issue

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -8,7 +8,7 @@ async function loginFormHandler(event) {
 
   if (email && password) {
     const response = await fetch("/api/users/login", {
-      method: "post",
+      method: "POST",
       body: JSON.stringify({
         email: email,
         password: password,
@@ -57,7 +57,7 @@ async function signUpFormHandler(event) {
       return;
     }
     const response = await fetch("/api/users", {
-      method: "post",
+      method: "POST",
       body: JSON.stringify({
         firstName: firstName,
         lastName: lastName,
